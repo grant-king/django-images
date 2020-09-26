19e9b5c2eb681c2204469dde4ffbc1679a6d0e97
Grant King <grantryan.king@gmail.com>
2020-09-25 17:34:42 -0700

    Add image detail page
    Add view, template, and path for image detial page.

--- mysite/imageprocessing/urls.py
+++ mysite/imageprocessing/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('image/<int:image_id>/', views.image_detail, name='image-detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/imageprocessing/views.py
+++ mysite/imageprocessing/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BaseImage
 
 def index(request):
     return HttpResponse("You are at the image processing app index.")
+
+def image_detail(request, image_id):
+    context = {
+        'base_image': BaseImage.objects.get(pk=image_id)
+        }
+    return render(request, 'imageprocessing/image_detail.html', context)
